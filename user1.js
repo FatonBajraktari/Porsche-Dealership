e292058c574b1c07950abc3b64e502d6b40477aa
Faton <fb67468@ubt-uni.net>
2023-12-08 23:16:59 +0100

    sign up validim

--- user1.js
+++ user1.js
@@ -6,7 +6,54 @@ function closeMenu() {
     document.getElementById("sideMenu").style.width = "0";
 }
 
+function validation(){
+    var name = document.getElementById('name');
+    var surname = document.getElementById('surname');
+    var email = document.getElementById('email');
+    var password = document.getElementById('password');
 
+    var nameError = document.getElementById('nameError');
+    var surnameError = document.getElementById('surnameError');
+    var emailError = document.getElementById('emailError');
+    var passwordError = document.getElementById('passwordError');
+
+
+
+
+    var nameRegex = /^[A-Z][a-z]+$/;
+    if(!nameRegex.test(name.value)){
+        nameError.innerText="Enter a valid name"
+        return;
+    }
+
+    var surnameRegex = /^[A-Z][a-z]+$/;
+    if(!surnameRegex.test(surname.value)){
+        surnameError.innerText = "Please enter a valid surname"
+        return;
+    }
+
+    var emailRegex = /^[a-zA-Z.-_]+@+[a-z]+\.+[a-z]{2,3}$/;
+    if(!emailRegex.test(email.value)){
+        emailError.innerText = "Enter a valid email";
+        return;
+    }
+
+    var passwordRegex = /^[A-Z][a-z]+(\d{3})$/;
+    if(!passwordRegex.test(password.value)){
+        passwordError.innerText = "Enter a password"
+        return;
+    }
+
+
+    var confirmRegex = /^[A-Z][a-z]+(\d{3})$/;
+    if(!passwordRegex.test(password.value)==confirmRegex.test(confirm.value)){
+        confirmError.innerText = "Passwords don't match"
+        return;
+    }
+
+    alert("You have been succesfully registered")
+
+}
 
 
 
