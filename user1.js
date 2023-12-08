ee61417bf6e5a8abe150ca021254fbdfa9e755b7
Faton <fb67468@ubt-uni.net>
2023-12-08 11:13:05 +0100

    changes

--- user1.js
+++ user1.js
@@ -1,48 +1,61 @@
-function validation(){
-    var name = document.getElementById('name');
-    var surname = document.getElementById('surname');
-    var email = document.getElementById('email');
-    var password = document.getElementById('password');
-    var password = document.getElementById('confirm');
+function openMenu() {
+    document.getElementById("sideMenu").style.width = "250px";
+}
 
-    var nameError = document.getElementById('nameError');
-    var surnameError = document.getElementById('surnameError');
-    var emailError = document.getElementById('emailError');
-    var passwordError = document.getElementById('passwordError');
-    var confirmError = document.getElementById('confirmError');
+function closeMenu() {
+    document.getElementById("sideMenu").style.width = "0";
+}
 
 
 
 
-    var nameRegex = /^[A-Z][a-z]+$/;
-    if(!nameRegex.test(name.value)){
-        nameError.innerText="Enter a valid name"
-        return;
-    }
 
-    var surnameRegex = /^[A-Z][a-z]+$/;
-    if(!surnameRegex.test(surname.value)){
-        surnameError.innerText = "Please enter a valid surname"
-        return;
-    }
 
-    var emailRegex = /^[a-zA-Z.-_]+@+[a-z]+\.+[a-z]{2,3}$/;
-    if(!emailRegex.test(email.value)){
-        emailError.innerText = "Enter a valid email";
-        return;
-    }
+// function validation(){
+//     var name = document.getElementById('name');
+//     var surname = document.getElementById('surname');
+//     var email = document.getElementById('email');
+//     var password = document.getElementById('password');
+//     var password = document.getElementById('confirm');
 
-    var passwordRegex = /^[A-Z][a-z]+(\d{5})$/;
-    if(!passwordRegex.test(password.value)){
-        passwordError.innerText = "Enter a password"
-        return;
-    }
+//     var nameError = document.getElementById('nameError');
+//     var surnameError = document.getElementById('surnameError');
+//     var emailError = document.getElementById('emailError');
+//     var passwordError = document.getElementById('passwordError');
+//     var confirmError = document.getElementById('confirmError');
+
+
+
+
+//     var nameRegex = /^[A-Z][a-z]+$/;
+//     if(!nameRegex.test(name.value)){
+//         nameError.innerText="Enter a valid name"
+//         return;
+//     }
+
+//     var surnameRegex = /^[A-Z][a-z]+$/;
+//     if(!surnameRegex.test(surname.value)){
+//         surnameError.innerText = "Please enter a valid surname"
+//         return;
+//     }
+
+//     var emailRegex = /^[a-zA-Z.-_]+@+[a-z]+\.+[a-z]{2,3}$/;
+//     if(!emailRegex.test(email.value)){
+//         emailError.innerText = "Enter a valid email";
+//         return;
+//     }
+
+//     var passwordRegex = /^[A-Z][a-z]+(\d{5})$/;
+//     if(!passwordRegex.test(password.value)){
+//         passwordError.innerText = "Enter a password"
+//         return;
+//     }
 
     
-    var confirmRegex = /^[A-Z][a-z]+(\d{5})$/;
-    if(confirmRegex==passwordRegex){
-        return;
-    }
+//     var confirmRegex = /^[A-Z][a-z]+(\d{5})$/;
+//     if(confirmRegex==passwordRegex){
+//         return;
+//     }
 
-    alert("Jeni regjistruar me sukses")
-}
+//     alert("Jeni regjistruar me sukses")
+// }
