b1ee4e6b53a864541b78bd3163720487196c42b1
Faton <fb67468@ubt-uni.net>
2023-12-08 09:38:18 +0100

    slideshow

--- models.js
+++ models.js
@@ -5,3 +5,51 @@ function openMenu() {
 function closeMenu() {
     document.getElementById("sideMenu").style.width = "0";
 }
+
+
+let i = 0;
+
+    let imgArray = ['911.jpg', '911.2.jpg', '911.3.jpg', '911.4.jpg', '911.5.jpg'];
+
+    function changeImg(){
+        document.getElementById('slideshow').src = imgArray[i];
+
+        if(i<imgArray.length-1){
+            i++;
+        }else{
+            i=0
+        }
+       
+    } document.addEventListener(onload, changeImg())
+
+
+    let n = 0;
+
+    let imgArray2 = ['panamera.jpg', 'panamera1.jpg', 'panamera2.jpg', 'panamera3.jpg', 'panamera4.jpg'];
+
+    function changeImg(){
+        document.getElementById('slideshow1').src = imgArray[i];
+
+        if(i<imgArray.length-1){
+            i++;
+        }else{
+            i=0
+        }
+       
+    } document.addEventListener(onload, changeImg())
+
+
+    let d = 0;
+
+    let imgArray3 = ['718.jpg', '718.1.jpg', '718.2.jpg', '718.3.jpg', '718.5.jpg'];
+
+    function changeImg(){
+        document.getElementById('slideshow2').src = imgArray[i];
+
+        if(i<imgArray.length-1){
+            i++;
+        }else{
+            i=0
+        }
+       
+    } document.addEventListener(onload, changeImg())
