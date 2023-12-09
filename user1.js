5d8374bb30ac9e0ba6ad5af59f1acfca9803e8b5
Faton <fb67468@ubt-uni.net>
2023-12-09 23:10:16 +0100

    changes

--- user1.js
+++ user1.js
@@ -11,64 +11,46 @@ function validation(){
     var surname = document.getElementById('surname');
     var email = document.getElementById('email');
     var password = document.getElementById('password');
-    // var confirm = document.getElementById('confirm');
+     var confirm = document.getElementById('confirm');
 
     var nameError = document.getElementById('nameError');
     var surnameError = document.getElementById('surnameError');
     var emailError = document.getElementById('emailError');
     var passwordError = document.getElementById('passwordError');
-    // var confirmError = document.getElementById('confirmError');
-
-
-
+    var confirmError = document.getElementById('confirmError');
 
     var nameRegex = /^[A-Z][a-z]+$/;
     if(!nameRegex.test(name.value)){
         nameError.innerText="Enter a valid name"
-        return;
+        return false;
     }
 
     var surnameRegex = /^[A-Z][a-z]+$/;
     if(!surnameRegex.test(surname.value)){
         surnameError.innerText = "Please enter a valid surname"
-        return;
+        return false;
     }
 
     var emailRegex = /^[a-zA-Z.-_]+@+[a-z]+\.+[a-z]{2,3}$/;
     if(!emailRegex.test(email.value)){
         emailError.innerText = "Enter a valid email";
-        return;
+        return false;
     }
-
-    // var passwordRegex = /^[A-Z][a-z]+(\d{3})$/;
-    // if(!passwordRegex.test(password.value)){
-    //     passwordError.innerText = "Enter a password"
-    //     return;
-    // }
-
-
-    // var confirmRegex = /^[A-Z][a-z]+(\d{3})$/;
-    // if(!passwordRegex.test(password.value)==confirmRegex.test(confirm.value)){
-    //     confirmError.innerText = "Passwords don't match"
-    //     return;
-    // }
 
     var passwordRegex = /^[A-Z][a-z]+(\d{3})$/;
 if (!passwordRegex.test(password.value)) {
     passwordError.innerText = "Enter a valid password";
-    return;
+    return false;
 }
 
-var confirmRegex = /^[A-Z][a-z]+(\d{3})$/;
-if (password.value !== confirm.value || !confirmRegex.test(confirm.value)) {
-    confirmError.innerText = "Passwords don't match";
-    return;
-}
+    if(password.value !== confirm.value){
+        confirmError.innerText="Passwords don't match";
+        return false;
+    }
+
 
     alert("You have been succesfully registered")
 
 }
-
-
 
 
