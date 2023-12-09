4c1d6f4fb4cfb628ee2d4f616bb8b68d0b6db4e3
epirotberisha <150926401+epirot1berisha@users.noreply.github.com>
2023-12-09 10:53:53 +0100

    changes

--- user.js
+++ user.js
@@ -5,3 +5,26 @@ function openMenu() {
 function closeMenu() {
     document.getElementById("sideMenu").style.width = "0";
 }
+
+
+function validation(){
+
+    var email = document.getElementById('email');
+    var password = document.getElementById('password');
+
+    var emailError = document.getElementById('emailError');
+    var passwordError = document.getElementById('passwordError');
+
+    
+var emailRegex = /^[a-zA-Z.-_]+@+[a-z]+\.+[a-z]{2,3}$/;
+    if(!emailRegex.test(email.value)){
+        emailError.innerText = "Enter a valid email";
+        return;
+    }
+
+    var passwordRegex = /^[A-Z][a-z]+(\d{3})$/;
+if (!passwordRegex.test(password.value)) {
+    passwordError.innerText = "Enter a valid password";
+    return;
+}
+}
