b6ccbd7efee7c5b3a027962e213f4a22b0291611
epirotberisha <150926401+epirot1berisha@users.noreply.github.com>
2023-12-09 10:44:31 +0100

    changes

--- user1.js
+++ user1.js
@@ -38,18 +38,30 @@ function validation(){
         return;
     }
 
+    // var passwordRegex = /^[A-Z][a-z]+(\d{3})$/;
+    // if(!passwordRegex.test(password.value)){
+    //     passwordError.innerText = "Enter a password"
+    //     return;
+    // }
+
+
+    // var confirmRegex = /^[A-Z][a-z]+(\d{3})$/;
+    // if(!passwordRegex.test(password.value)==confirmRegex.test(confirm.value)){
+    //     confirmError.innerText = "Passwords don't match"
+    //     return;
+    // }
+
     var passwordRegex = /^[A-Z][a-z]+(\d{3})$/;
-    if(!passwordRegex.test(password.value)){
-        passwordError.innerText = "Enter a password"
-        return;
-    }
+if (!passwordRegex.test(password.value)) {
+    passwordError.innerText = "Enter a valid password";
+    return;
+}
 
-
-    var confirmRegex = /^[A-Z][a-z]+(\d{3})$/;
-    if(!passwordRegex.test(password.value)==confirmRegex.test(confirm.value)){
-        confirmError.innerText = "Passwords don't match"
-        return;
-    }
+var confirmRegex = /^[A-Z][a-z]+(\d{3})$/;
+if (password.value !== confirm.value || !confirmRegex.test(confirm.value)) {
+    confirmError.innerText = "Passwords don't match";
+    return;
+}
 
     alert("You have been succesfully registered")
 
@@ -58,51 +70,3 @@ function validation(){
 
 
 
-// function validation(){
-//     var name = document.getElementById('name');
-//     var surname = document.getElementById('surname');
-//     var email = document.getElementById('email');
-//     var password = document.getElementById('password');
-//     var password = document.getElementById('confirm');
-
-//     var nameError = document.getElementById('nameError');
-//     var surnameError = document.getElementById('surnameError');
-//     var emailError = document.getElementById('emailError');
-//     var passwordError = document.getElementById('passwordError');
-//     var confirmError = document.getElementById('confirmError');
-
-
-
-
-//     var nameRegex = /^[A-Z][a-z]+$/;
-//     if(!nameRegex.test(name.value)){
-//         nameError.innerText="Enter a valid name"
-//         return;
-//     }
-
-//     var surnameRegex = /^[A-Z][a-z]+$/;
-//     if(!surnameRegex.test(surname.value)){
-//         surnameError.innerText = "Please enter a valid surname"
-//         return;
-//     }
-
-//     var emailRegex = /^[a-zA-Z.-_]+@+[a-z]+\.+[a-z]{2,3}$/;
-//     if(!emailRegex.test(email.value)){
-//         emailError.innerText = "Enter a valid email";
-//         return;
-//     }
-
-//     var passwordRegex = /^[A-Z][a-z]+(\d{5})$/;
-//     if(!passwordRegex.test(password.value)){
-//         passwordError.innerText = "Enter a password"
-//         return;
-//     }
-
-    
-//     var confirmRegex = /^[A-Z][a-z]+(\d{5})$/;
-//     if(confirmRegex==passwordRegex){
-//         return;
-//     }
-
-//     alert("Jeni regjistruar me sukses")
-// }
